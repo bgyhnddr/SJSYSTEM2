10129f1eb7e01bd66c912204188e1122c4dc55ab
guanyi.bei <guanyi.bei@cyphertech.cn>
2016-12-12 11:24:15 +0800

    将初始化脚本弄在node命令内
    Signed-off-by: guanyi.bei <guanyi.bei@cyphertech.cn>

--- src/server/entry.js
+++ src/server/entry.js
@@ -4,10 +4,6 @@ var bodyParser = require('body-parser')
 
 
 module.exports = (app) => {
-  app.use('/init', function(req, res, next) {
-    var init = require('../db/init')
-    init(req, res, next)
-  })
 
   // parse application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({
